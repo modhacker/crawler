69e042b4330d6c01c30be25ad8583592a50118fd
JongSuk, JANG <modhacker@knou.ac.kr>
2024-01-13 15:24:57 +0900

    [Update] Change keyword

--- 10_selenium/ex05_search.py
+++ 10_selenium/ex05_search.py
@@ -10,9 +10,9 @@ driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install())
 # 접속
 driver.get("https://www.naver.com")
 
-# 검색창에 파이썬이라고 입력하고 검색버튼 클릭해보기
+# 검색창에 오늘 날씨이라고 입력하고 검색버튼 클릭해보기
 search = driver.find_element(By.XPATH, '//*[@id="query"]')
-search.send_keys("파이썬")
+search.send_keys("오늘 날씨")
 # 입력하는 것 까지 확인하고 검색 버튼 클릭
 search_button = driver.find_element(By.XPATH, '//*[@id="search-btn"]')
 search_button.click()
